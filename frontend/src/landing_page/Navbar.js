b82b2edd2fbf8d8dbd62423668a06753e16d9dee
Aman Singh <as588895@gmail.com>
2026-07-18 05:50:03 +0530

    updated navbar section

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
             <nav class="navbar navbar-expand-lg border-bottom" style={{backgroundColor: "#fff"}}>
                 <div class="container p-2">
-                    <a class="navbar-brand" href="#">
+                    <Link class="navbar-brand" to="/">
                         <img src='media/images/logo.svg' alt="logo" style={{ width: '25%' }} />
-                    </a>
+                    </Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -16,19 +17,19 @@ function Navbar() {
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Signup</a>
+                                <Link class="nav-link active" aria-current="page" to="/signup">Signup</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" href="#">About</a>
+                                <Link class="nav-link active" aria-current="page" to="/about">About</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" href="#">Product</a>
+                                <Link class="nav-link active" aria-current="page" to="/products">Product</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" href="#">Pricing</a>
+                                <Link class="nav-link active" aria-current="page" to="/pricing">Pricing</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" href="#">Support</a>
+                                <Link class="nav-link active" aria-current="page" to="/supports">Support</Link>
                             </li>
                            
                         </ul>
